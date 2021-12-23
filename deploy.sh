e7f55cbd9b2aa1a443c9659ff43b89affe8416d1
Artem Titoulenko <artem.titoulenko@gmail.com>
2021-12-23 13:18:28 -0500

    pipe pipe pipe

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+COMMIT="$(git rev-parse --short HEAD)"
+TAG="www.artem.fans:$COMMIT"
+
 set -x
-docker-compose -f docker-compose.yml up -d --build
+TAG=$TAG docker-compose -f docker-compose.yml up --build -d --remove-orphans 
