40b5eb4610f04a68ee236b819795428916c630b3
Artem Titoulenko <artem.titoulenko@gmail.com>
2021-12-23 12:11:16 -0500

    love to play with deploy pipeline

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ COMMIT="$(git rev-parse --short HEAD)"
 TAG="www.artem.fans:$COMMIT"
 echo "Deploying $TAG"
 set -x
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml up -d --build-arg TAG=$TAG
